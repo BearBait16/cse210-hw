9072de2529a602a40614c7f11c0e1b81eba73cb1
BearBait16 <jacksonaden@gmail.com>
2024-01-13 16:32:38 -0700

    Finished Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is the grade percentage? ");
+        string gradeString = Console.ReadLine();
+        float gradeFloat = float.Parse(gradeString);
+
+        bool passBool = true;
+
+        string grade = "A";
+
+        if (gradeFloat >= 90)
+        {
+            grade = "A";
+        }
+        else if (gradeFloat >= 80)
+        {
+            grade = "B";
+        }
+        else if (gradeFloat >= 70)
+        {
+            grade = "C";
+        }
+        else if (gradeFloat >= 60)
+        {
+            grade = "D";
+            passBool = false;
+        }
+        else if (gradeFloat < 60)
+        {
+            grade = "F";
+            passBool = false;
+        }
+
+        Console.WriteLine(grade);
+
+        if (passBool == true)
+        {
+            Console.WriteLine($"You passed the class");
+        }
+        else if (passBool == false)
+        {
+            Console.WriteLine($"You failed the class");
+        }
+
     }
 }
