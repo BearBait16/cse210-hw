f2e5548209d19f158ff9d5ec63079650c1fe56bb
BearBait16 <jacksonaden@gmail.com>
2024-01-18 12:11:21 -0700

    Finished Prep 3 and Prep 4

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int random = randomGenerator.Next(1, 100);
+        bool correct = false;
+        int numGuesses = 0;
+
+        do
+        {
+            System.Console.WriteLine("What is your guess?");
+            int guess = int.Parse(Console.ReadLine());
+
+            if (random < guess)
+            {
+                System.Console.WriteLine("Your number is too high, try agian");
+            }
+            else if (random > guess)
+            {
+                System.Console.WriteLine("Your number is too low, try agian");
+            }
+            else if (random == guess)
+            {
+                System.Console.WriteLine("Correct!");
+                correct = true;
+            }
+            numGuesses++;
+        } while (correct != true);
+
+        System.Console.WriteLine($"You guessed {numGuesses} times.");
+
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> userInts = new List<int>();
+        bool finishLoop = false;
+        int largestInt = 0;
+
+
+        while (finishLoop != true)
+        {
+            Console.WriteLine("Input integer, input 0 if finished. ");
+            int numNew = int.Parse(Console.ReadLine());
+            if (numNew > largestInt)
+            {
+                largestInt = numNew;
+            }
+            if (numNew != 0)
+            {
+                userInts.Add(numNew);
+            }
+            else if (numNew == 0)
+            {
+                finishLoop = true;
+            }
+        }
+        double averageInt = userInts.Average();
+        int sumInt = userInts.Sum();
+        System.Console.WriteLine($"The Sum is {sumInt}");
+        System.Console.WriteLine($"The Average of the numbers is {averageInt}");
+        System.Console.WriteLine($"The Largest number in the list is {largestInt}");
+
     }
 }
