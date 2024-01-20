95fb55e78b67293b91c430c0e06a3ff7514d445f
BearBait16 <jacksonaden@gmail.com>
2024-01-19 18:56:40 -0700

    Finished Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        System.Console.Write("Give me one more integer! ");
+        int newNumber = int.Parse(Console.ReadLine());
+        int product = PromptSquareNumber(newNumber);
+        DisplayResult(product, userName);
+
+        static void DisplayWelcome()
+        {
+            System.Console.WriteLine("Welcome to the program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("What is your name? ");
+            string userName = Console.ReadLine();
+            System.Console.WriteLine($"Nice to meet you {userName}");
+            return userName;
+        }
+        static int PromptUserNumber()
+        {
+            System.Console.Write("What is your favorite integer? ");
+            int favNum = int.Parse(Console.ReadLine());
+            System.Console.WriteLine($"{favNum} is my favorite too!");
+            return favNum;
+        }
+
+
+
+        static int PromptSquareNumber(int newNumber)
+        {
+            int product = newNumber * newNumber;
+            return product;
+        }
+        static void DisplayResult(int product, string userName)
+        {
+            System.Console.WriteLine($"{userName}, the square of your number is {product}.");
+        }
     }
 }
